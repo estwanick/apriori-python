24c9fd30ebfcc3be40a4c14a97f9594545d459ec
estwanick <estwanick@gmail.com>
2018-02-16 22:50:10 -0500

    initial db setup

---
+++ db/setup.sh
@@ -0,0 +1,9 @@
+find . -name "*.db" -type f -delete
+
+sqlite3 kmart.db < ./sql/populate-kmart.sql;
+sqlite3 amazon.db < ./sql/populate-amazon.sql;
+sqlite3 shoprite.db < ./sql/populate-shoprite.sql;
+sqlite3 walmart.db < ./sql/populate-walmart.sql;
+sqlite3 wawa.db < ./sql/populate-wawa.sql;
+
+find . -name "*.db" -type f
---
+++ db/sql/populate-wawa.sql
@@ -0,0 +1,116 @@
+PRAGMA foreign_keys = ON;
+.headers on
+.mode column
+
+drop table if exists Items;
+drop table if exists Transactions;
+drop table if exists Orders;
+
+create table Items(
+    id integer primary key,
+    item text
+);
+
+create table Orders(
+    id integer primary key
+);
+
+create table Transactions(
+    id integer,
+    item_id integer,
+    foreign key (id) references Orders(id),
+    foreign key (item_id) references Items(id)
+);
+
+insert into Items (id, item) values (0, 'milk');
+insert into Items (id, item) values (1, 'bread');
+insert into Items (id, item) values (2, 'soda');
+insert into Items (id, item) values (3, 'beer');
+insert into Items (id, item) values (4, 'ice');
+insert into Items (id, item) values (5, 'coffee');
+insert into Items (id, item) values (6, 'chips');
+insert into Items (id, item) values (7, 'salsa');
+insert into Items (id, item) values (8, 'yogurt');
+insert into Items (id, item) values (9, 'apples');
+
+insert into Orders (id) values (0);
+insert into Orders (id) values (1);
+insert into Orders (id) values (2);
+insert into Orders (id) values (3);
+insert into Orders (id) values (4);
+insert into Orders (id) values (5);
+insert into Orders (id) values (6);
+insert into Orders (id) values (7);
+insert into Orders (id) values (8);
+insert into Orders (id) values (9);
+insert into Orders (id) values (10);
+insert into Orders (id) values (11);
+insert into Orders (id) values (12);
+insert into Orders (id) values (13);
+insert into Orders (id) values (14);
+insert into Orders (id) values (15);
+insert into Orders (id) values (16);
+insert into Orders (id) values (17);
+insert into Orders (id) values (18);
+insert into Orders (id) values (19);
+
+insert into Transactions (id, item_id) values (0, 0);
+insert into Transactions (id, item_id) values (0, 1);
+insert into Transactions (id, item_id) values (0, 2);
+insert into Transactions (id, item_id) values (1, 1);
+insert into Transactions (id, item_id) values (1, 0);
+insert into Transactions (id, item_id) values (1, 2);
+insert into Transactions (id, item_id) values (2, 2);
+insert into Transactions (id, item_id) values (2, 3);
+insert into Transactions (id, item_id) values (2, 2);
+insert into Transactions (id, item_id) values (3, 5);
+insert into Transactions (id, item_id) values (3, 6);
+insert into Transactions (id, item_id) values (3, 7);
+insert into Transactions (id, item_id) values (4, 7);
+insert into Transactions (id, item_id) values (4, 8);
+insert into Transactions (id, item_id) values (4, 9);
+insert into Transactions (id, item_id) values (5, 1);
+insert into Transactions (id, item_id) values (5, 0);
+insert into Transactions (id, item_id) values (5, 2);
+insert into Transactions (id, item_id) values (6, 0);
+insert into Transactions (id, item_id) values (6, 1);
+insert into Transactions (id, item_id) values (6, 3);
+insert into Transactions (id, item_id) values (7, 0);
+insert into Transactions (id, item_id) values (7, 1);
+insert into Transactions (id, item_id) values (7, 3);
+insert into Transactions (id, item_id) values (8, 0);
+insert into Transactions (id, item_id) values (8, 1);
+insert into Transactions (id, item_id) values (8, 2);
+insert into Transactions (id, item_id) values (9, 5);
+insert into Transactions (id, item_id) values (9, 7);
+insert into Transactions (id, item_id) values (9, 9);
+insert into Transactions (id, item_id) values (10, 0);
+insert into Transactions (id, item_id) values (10, 1);
+insert into Transactions (id, item_id) values (10, 2);
+insert into Transactions (id, item_id) values (11, 1);
+insert into Transactions (id, item_id) values (11, 0);
+insert into Transactions (id, item_id) values (11, 2);
+insert into Transactions (id, item_id) values (12, 2);
+insert into Transactions (id, item_id) values (12, 3);
+insert into Transactions (id, item_id) values (12, 2);
+insert into Transactions (id, item_id) values (13, 5);
+insert into Transactions (id, item_id) values (13, 6);
+insert into Transactions (id, item_id) values (13, 7);
+insert into Transactions (id, item_id) values (14, 7);
+insert into Transactions (id, item_id) values (14, 8);
+insert into Transactions (id, item_id) values (14, 9);
+insert into Transactions (id, item_id) values (15, 1);
+insert into Transactions (id, item_id) values (15, 0);
+insert into Transactions (id, item_id) values (15, 2);
+insert into Transactions (id, item_id) values (16, 0);
+insert into Transactions (id, item_id) values (16, 1);
+insert into Transactions (id, item_id) values (16, 3);
+insert into Transactions (id, item_id) values (17, 0);
+insert into Transactions (id, item_id) values (17, 1);
+insert into Transactions (id, item_id) values (17, 3);
+insert into Transactions (id, item_id) values (18, 0);
+insert into Transactions (id, item_id) values (18, 1);
+insert into Transactions (id, item_id) values (18, 2);
+insert into Transactions (id, item_id) values (19, 5);
+insert into Transactions (id, item_id) values (19, 7);
+insert into Transactions (id, item_id) values (19, 9);
