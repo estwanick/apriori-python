36cd9f73f2faaee90f3172854f0744ca08bd0853
estwanick <estwanick@gmail.com>
2018-02-22 23:53:10 -0500

    add support for command line arguments

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
 from utils.apriori import apriori
 from utils.db import fetch_db
 from utils.format import print_results
+import argparse
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument('--support', help='Support threshold')
+parser.add_argument('--confidence', help='Confidence thrshold')
 
 amazon =  fetch_db('amazon.db')
 kmart =  fetch_db('kmart.db')
@@ -8,8 +14,10 @@ shoprite =  fetch_db('shoprite.db')
 walmart =  fetch_db('walmart.db')
 wawa =  fetch_db('wawa.db')
 
-support = .5
-confidence = .5
+args=parser.parse_args()
+
+support = float(args.support) # TODO: Resolve error caused by passing cmd arg
+confidence = float(args.confidence)
 
 print_results(
     'Amazon',
